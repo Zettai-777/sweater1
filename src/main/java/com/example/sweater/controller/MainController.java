edf59168ec3354b937752e79e8bfbc532006b8ae
Zettai-777 <zettai777@gmail.com>
2021-02-24 01:56:18 +0300

    Init commit. Spring Boot Application.

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,69 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting() {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+//    @DeleteMapping("main")
+//    public String delete(@RequestParam Long id, Map<String, Object> model){
+//        Optional<Message> messageById = messageRepo.findById(id);
+//        if(messageById.isPresent()){
+//            Message message = messageById.get();
+//            messageRepo.delete(message);
+//            model.remove(message);
+//        }
+//        return "main";
+//    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
+    }
+
+
+
+}
---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,49 @@
+package com.example.sweater.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String text;
+
+    @Column
+    private String tag;
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Message() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
